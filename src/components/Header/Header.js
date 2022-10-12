51e4eb9348a2ba602a23a1d812dbebdf61e9a6d5
Rahat-Ul-Anwar <anwarrahat69@gmail.com>
2022-10-12 11:30:42 +0600

    some changes

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import img from '../../images/side-img.png';
+import './Header.css';
 
 const Header = () => {
     return (
-        <div className='grid grid-cols-2 gap-5 items-center justify-between mt-0 p-10 bg-yellow-100'>
-            <div>
+        <div className='grid grid-cols-2 gap-5 items-center justify-between mb-10 p-10 bg-yellow-100'>
+            <div className='side-img'>
               <img src={img} alt="" srcset="" />
                     
             </div>
 
             <div>
-                <h2 className='text-2xl font-medium'> This is the quiz application app where you can find technology related quiz such as HTML, CSS, JavaScript, Git and many more. This app is very helpful for the beginners..</h2>
+                <h2 className='text-xl font-medium'> This is the quiz application app where you can find technology related quiz such as HTML, CSS, JavaScript, Git and many more. This app is very helpful for the beginners..</h2>
             </div>
              
         </div>
--- src/components/Topic/Topic.js
+++ src/components/Topic/Topic.js
@@ -3,7 +3,8 @@ import { useNavigate } from 'react-router-dom';
 import './Topic.css';
 
 const Topic = ({ topic }) => {
-    const { id, logo, name } = topic;
+    const { id, logo, name,total } = topic;
+    console.log(topic)
     const navigate = useNavigate();
 
     const handleClick = () => {
@@ -13,8 +14,10 @@ const Topic = ({ topic }) => {
 
     return (
         <div className='bg-slate-100 w-80 p-4'>
-            <img src={logo} alt="" className='p-3'/>
-            <div className='flex items-center justify-between'>
+            <img src={logo} alt="" className='p-3' />
+            <h3 className='text-xl'>Total Question: {total}</h3>
+            
+            <div className='flex items-center justify-between my-5'>
             <h3 className='text-2xl font-bold'>{name}</h3>
             <button onClick={handleClick} >Start Practice</button>
 
